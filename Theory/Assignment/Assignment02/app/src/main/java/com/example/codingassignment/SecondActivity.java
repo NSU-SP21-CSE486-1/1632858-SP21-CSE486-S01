0171d3ee982928e84753f7b7ee80c77c19e19d9f
Sadman Ahmed <sdmn.ahmd@gmail.com>
2021-04-07 23:32:00 +0600

    Design #29: Designed some more views of the layout and fixed some issue
    The issue with the icon of the date of birth field is fixed.
    Gender selector drop down menu is selected

--- Theory/Assignment/Assignment02/app/src/main/java/com/example/codingassignment/SecondActivity.java
+++ Theory/Assignment/Assignment02/app/src/main/java/com/example/codingassignment/SecondActivity.java
@@ -4,40 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
-import android.widget.Toast;
 
 import static android.R.layout.simple_list_item_1;
 
 public class SecondActivity extends AppCompatActivity {
     private AutoCompleteTextView mSchoolNameList;
     private AutoCompleteTextView mDepartmentNameList;
+    private AutoCompleteTextView mGenderList;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        mSchoolNameList = findViewById(R.id.school_name_view);
-        mDepartmentNameList = findViewById(R.id.dep_name_list);
+        mSchoolNameList = findViewById(R.id.school_name_dropdown);
+        mDepartmentNameList = findViewById(R.id.dep_name_dropdown);
+        mGenderList = findViewById(R.id.gender_list_dropdwon);
 
         String[] schoolNameList = getResources().getStringArray(R.array.school_name);
+
         String[] depName1List = getResources().getStringArray(R.array.depName_1);
         String[] depName2List = getResources().getStringArray(R.array.depName_2);
         String[] depName3List = getResources().getStringArray(R.array.depName_3);
 
+        String[] genderList = getResources().getStringArray(R.array.gender_list);
 
         ArrayAdapter schoolNameAdapter = new ArrayAdapter(this, simple_list_item_1, schoolNameList);
+
         ArrayAdapter depName1Adapter = new ArrayAdapter(this, simple_list_item_1, depName1List);
         ArrayAdapter depName2Adapter = new ArrayAdapter(this, simple_list_item_1, depName2List);
         ArrayAdapter depName3Adapter = new ArrayAdapter(this, simple_list_item_1, depName3List);
 
-        mSchoolNameList.setAdapter(schoolNameAdapter);
+        ArrayAdapter genderAdapter = new ArrayAdapter(this, simple_list_item_1, genderList);
 
+        mSchoolNameList.setAdapter(schoolNameAdapter);
+        mGenderList.setAdapter(genderAdapter);
         mSchoolNameList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
