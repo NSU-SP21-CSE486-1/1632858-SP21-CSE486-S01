379c620311a27fae0fd9698a2c11cf9240bb6f68
Sadman Ahmed <sdmn.ahmd@gmail.com>
2021-05-06 14:39:26 +0600

    Code #47: Created the layout of the Login page
    Layout of the login page complete with added image.

---
+++ Theory/Assignment/Assignment03/FirebaseCodingAssignment/app/src/main/java/com/example/firebasecodingassignment/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.firebasecodingassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginClick(View view) {
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+    }
+}
