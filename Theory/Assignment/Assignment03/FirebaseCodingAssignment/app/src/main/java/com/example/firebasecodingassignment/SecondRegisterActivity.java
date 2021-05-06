71e58f60b5eb928cfcfb1ccfc4433e5eccbba90e
Sadman Ahmed <sdmn.ahmd@gmail.com>
2021-05-06 16:25:25 +0600

    Code #47: Created the Register Page
    The register is divided into two activities.

---
+++ Theory/Assignment/Assignment03/FirebaseCodingAssignment/app/src/main/java/com/example/firebasecodingassignment/SecondRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.firebasecodingassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_register);
+    }
+}
