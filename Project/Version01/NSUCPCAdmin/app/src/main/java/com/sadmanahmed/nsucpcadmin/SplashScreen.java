28fecc1c5f38e014954bb7ec88a739a49cd66507
Sadman Ahmed <sdmn.ahmd@gmail.com>
2021-05-18 10:28:20 +0600

    Design Code #54 #55: Created the splash screen for the Employer app.
    Created the layout of the Employer app and set a 4 second delay to show the total screen.

---
+++ Project/Version01/NSUCPCAdmin/app/src/main/java/com/sadmanahmed/nsucpcadmin/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.sadmanahmed.nsucpcadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //splashscreen show delay
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        },4000);
+    }
+}
