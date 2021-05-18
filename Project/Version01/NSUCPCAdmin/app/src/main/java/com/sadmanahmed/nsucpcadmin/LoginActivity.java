d5607ce3281a0e1705d89024f87afe2cd9a2d90e
Sadman Ahmed <sdmn.ahmd@gmail.com>
2021-05-18 12:43:38 +0600

    Code #54 #55: Made some tweaks in the layout

--- Project/Version01/NSUCPCAdmin/app/src/main/java/com/sadmanahmed/nsucpcadmin/LoginActivity.java
+++ Project/Version01/NSUCPCAdmin/app/src/main/java/com/sadmanahmed/nsucpcadmin/LoginActivity.java
@@ -2,13 +2,28 @@ package com.sadmanahmed.nsucpcadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Window window;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //set status bar color
+        if(Build.VERSION.SDK_INT >= 21){
+            window = this.getWindow();
+            window.setStatusBarColor(this.getResources().getColor(R.color.topbar_color));
+        }
+    }
+
+    public void onLoginBtnClick(View view) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- Project/Version01/NSUCPCAdmin/app/src/main/java/com/sadmanahmed/nsucpcadmin/SplashScreen.java
+++ Project/Version01/NSUCPCAdmin/app/src/main/java/com/sadmanahmed/nsucpcadmin/SplashScreen.java
@@ -3,23 +3,31 @@ package com.sadmanahmed.nsucpcadmin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.Window;
 import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
-
+    Window window;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        //set status bar color
+        if(Build.VERSION.SDK_INT >= 21){
+            window = this.getWindow();
+            window.setStatusBarColor(this.getResources().getColor(R.color.topbar_color));
+        }
+
         //splashscreen show delay
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
                 startActivity(intent);
             }
         },4000);
