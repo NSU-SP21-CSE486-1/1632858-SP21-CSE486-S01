bfa244607ed7d25f4f46ed87e05836fc5c31639d
Sadman Ahmed <sdmn.ahmd@gmail.com>
2021-05-19 11:09:12 +0600

    Code #54 #55: Created the layout for the Main Screen
    Created the Main screen layout and also added the option menu in the main screen action bar and also set the on click methods for the option menu.

--- Project/Version01/NSUCPCAdmin/app/src/main/java/com/sadmanahmed/nsucpcadmin/MainActivity.java
+++ Project/Version01/NSUCPCAdmin/app/src/main/java/com/sadmanahmed/nsucpcadmin/MainActivity.java
@@ -1,8 +1,16 @@
 package com.sadmanahmed.nsucpcadmin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set the action bar title
+        getSupportActionBar().setTitle(R.string.welcome);
+
+        //set the actionbar color
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#E9D36B")));
+    }
+
+    //Set the options menu on the action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.mainscreen_menu, menu);
+        return true;
+    }
+
+    //choose the onclick for the option menu items
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mainmenu_option1:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.mainmenu_option2:
+                Toast.makeText(this, "Add Jobs", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.mainmenu_option3:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.mainmenu_option4:
+                Toast.makeText(this, "All Jobs", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.mainmenu_option5:
+                Toast.makeText(this, "Change Password", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.mainmenu_option6:
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
